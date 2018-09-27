1d6286f92d1dad3e6ad59e47900a78a2140d8b5a
szogik <40832662+szogik@users.noreply.github.com>
2018-09-27 19:54:19 +0200

    test
    Initial commit

---
+++ minineptun/src/main/java/hu/elte/backend/minineptun/MinineptunApplication.java
@@ -0,0 +1,12 @@
+package hu.elte.backend.minineptun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinineptunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinineptunApplication.class, args);
+	}
+}
---
+++ minineptun/src/test/java/hu/elte/backend/minineptun/MinineptunApplicationTests.java
@@ -0,0 +1,16 @@
+package hu.elte.backend.minineptun;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MinineptunApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
